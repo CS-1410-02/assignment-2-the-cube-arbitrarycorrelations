9890ecce52462b665ddd868917f1eeec680cc850
arbitrarycorrelations <subsystem@GS65.localdomain>
2022-09-16 12:47:54 -0600

    Further implemented the Face class and created the Cube hashmap as well as the printCube function.

--- Cube.java
+++ Cube.java
@@ -1,30 +1,58 @@
-
+import java.util.HashMap;
+import java.util.Map;
 public class Cube {
 	public static void main(String[] args){
-    char[] colors = {'r','b','o','g','y','w'};
-    //cube object will be list of Face objects
+    //must take in arguments
+    char[] colors_list = {'r','b','o','g','y','w'};
+    Map<Integer, Face> cube = new HashMap<Integer, Face>(); //cube hashmap of int :: Face
     
-
+    for(int i = 0; i < 6; i++){
+      cube.put(i, new Face(colors_list[i]));
+    }
+    
+    printCube(cube);
   }
 
+  public static void printCube(Map<Integer, Face> cube){
+    for(Face value: cube.values()){
+      value.getFace();
+      System.out.println();
+    }
+  }
 }
 
 class Face {
   private char padding = '|';
-  private String[][] face_matrix = new String[3][5];
+  private String[][] face_matrix = new String[3][5]; 
+  private char facecolor;
+  /*This class implementation works because every created Face object starts with a blank
+   * face_matrix that is then set on instantiation
+   */
   Face(char color){
+    this.facecolor = color;
     for(int x = 0; x < 3; x++){ //Columns
-      for (int y = 0; y < 5; y++){ //Rows
-        if (y != 1 && y != 3){face_matrix[x][y] = Character.toString(color);}
-        else {face_matrix[x][y] = Character.toString(padding);}
+      for (int y = 0; y < 3; y++){ //Rows
+        face_matrix[x][y] = Character.toString(color);
       }
     }
   
   }
 
-  public String getFace(){
+  //public char getFaceColor(){return this.facecolor;}
 
-    return "";
+  public void getFace(){
+    String[][] target_data = this.face_matrix; 
+    //add padding here instead of clogging up the actual face_matrix
+    String blank = "";
+    for(int row = 0; row < 3; row++){
+      if(row > 0){blank += '\n';}
+      for(int column = 0; column < 3; column++){
+        blank += Character.toString(this.facecolor);
+        if(column == 0 || column == 1){blank += '|';}
+      }
+    }
+    System.out.println(blank);
+    
   }
 
   public String modifyFace(int column, int row, char newColor){
